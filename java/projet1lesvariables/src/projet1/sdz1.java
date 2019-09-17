b8eb2a20d38ca88d8fc8fcb2da521f6ce4f56740
ahmed0000-jpg <ahmedbelaiba19952018@gmail.com>
2019-09-17 03:06:52 +0100

    les variables en java

---
+++ java/projet1lesvariables/src/projet1/sdz1.java
@@ -0,0 +1,22 @@
+package projet1;
+
+public class sdz1 {
+
+	public static void main(String[] args) {
+		
+		int temperatureSoleil; // declaration
+		temperatureSoleil=15600000; // initialisation
+		
+		char caractere;
+		caractere='A';
+		
+		boolean question;
+		question=true;
+		
+		String phrase=new String("votre chaine de caractere");
+		System.out.println(phrase);
+		
+
+	}
+
+}
